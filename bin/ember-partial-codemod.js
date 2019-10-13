b38499af7ee5f3ed4ad7017cb92baeb514820ffd
Yicheng Gong <yigong@yigong-mn4.linkedin.biz>
2019-10-13 00:23:28 -0700

    Fixed some component construction pathing and added --help

--- bin/ember-partial-codemod.js
+++ bin/ember-partial-codemod.js
@@ -6,6 +6,7 @@ const fse = require("fs-extra");
 const { transformPartial } = require("../lib/transform-partial");
 const { gatherPartialInfo } = require("../lib/gather-partial-info");
 const { generateComponent } = require("../lib/generate-component");
+const DELIMITERS = require("../lib/constant/delimiters");
 
 /**
  * 1. Recast: go through each `partial parent`, and use  `partialModuleNameAttributeMap` to transform any `partials`.
@@ -13,14 +14,34 @@ const { generateComponent } = require("../lib/generate-component");
  */
 function run() {
   const argv = yargs
-    .alias("v", "verbose")
-    .alias("t", "transform")
-    .alias("crr", "component-replace")
-    .alias("rd", "replace-delimiter")
+    .help("help")
+    .alias("h", "help")
+    .options({
+      verbose: {
+        alias: "verb",
+        description: "Display useful information while running codemod",
+        type: "boolean"
+      },
+      transform: {
+        alias: "t",
+        description: "Custom transform module for recasting",
+        type: "string"
+      },
+      componentReplaceRegex: {
+        alias: "crr",
+        description: "Regex to convert template to component path",
+        type: "string"
+      },
+      replaceDelimiter: {
+        alias: "rd",
+        description: `Replace module name [${DELIMITERS}] while recasting`,
+        type: "string"
+      }
+    })
     .boolean("verbose")
     .choices("replace-delimiter", ["$", "::", "@"])
     .argv;
-  const { transform: customTransform, replaceDelimiter, verbose, componentReplace } = argv;
+  const { transform: customTransform, replaceDelimiter, verbose, componentReplaceRegex } = argv;
 
   if (!verbose) {
     console.info = () => {};
@@ -39,7 +60,14 @@ function run() {
     const template = fs.readFileSync(partialParentsPhysicalDiskPath).toString();
     const { code: newTemplate, attributes: attrs } = transform(template, partialModuleNameAttributeMap, { replaceDelimiter });
     if (newTemplate && (template !== newTemplate)) {
-      const componentPhysicalDiskPath = componentReplace ? partialParentsPhysicalDiskPath.replace(crr) : partialParentsPhysicalDiskPath.replace("/templates/", "/");
+      let componentPhysicalDiskPath = partialParentsPhysicalDiskPath;
+      if (componentReplaceRegex) {
+        componentPhysicalDiskPath = componentPhysicalDiskPath.replace(componentReplaceRegex);
+      } else if (componentPhysicalDiskPath.includes("/components/")) {
+        componentPhysicalDiskPath = componentPhysicalDiskPath.replace("/templates/", "/");
+      } else {
+        componentPhysicalDiskPath = componentPhysicalDiskPath.replace("/templates/", "/components/");
+      }
       const component = generateComponent(attrs);
       // If the component file does not exist and we need to create one, we create one.
       if (!fs.existsSync(componentPhysicalDiskPath) && component) {
